a3d437c5f37057fbd6156babe04fb830abbc24ee
Nikolai Nekrasov <nikolainekrasov1999@tessalite.com>
2025-06-11 03:18:21 +0300

    feat: create screen manager

---
+++ Scripts/ScreenManager.gd
@@ -0,0 +1,25 @@
+extends Control
+
+class_name ScreenManager
+
+var screens = {}
+var active_screen: CanvasLayer
+var prev_screen: CanvasLayer = null
+
+
+func _ready():
+	for screen in get_children():
+		screens[screen.name] = screen
+	active_screen = screens["Main menu"]
+	
+func show_screen(screen_name):
+	prev_screen = active_screen
+	active_screen.visible = false
+	active_screen = screens[screen_name]
+	active_screen.visible = true
+
+func show_main_menu():
+	show_screen("Main menu")
+	
+func exit_game():
+	get_tree().quit()
