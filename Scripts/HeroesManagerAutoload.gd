590d430a1621d6b568044df19de7480915c0ac0d
Nikolai Nekrasov <nikolainekrasov@tessalite.com>
2025-07-15 00:06:12 +0300

    feat: Move hero creation to the moment of starting a new game

--- Scripts/HeroesManagerAutoload.gd
+++ Scripts/HeroesManagerAutoload.gd
@@ -4,7 +4,9 @@ extends Node
 class_name HeroesManagerAutoload
 
 var heroes = []
-var heroes_info_json = null
+var strength_heroes = []
+var agility_heroes = []
+var intelligence_heroes = []
 var gain_per_level_coef: float = 1.0 / 10.0
 var base_health_coef: int = 10
 var hp_regen_coef: float = 1.0 / 10.0
@@ -24,20 +26,24 @@ var attr_coefs = {
 	"base_move_speed": 10,
 	"base_attack_range": 1
 }
-
-func _ready():
+	
+func setup_heroes_for_new_game():
+	delete_all_heroes()
 	var heroes_info = FileAccess.open("res://Heroes//Heroes Generation Info.json", FileAccess.READ)
 	var content = heroes_info.get_as_text()
-	heroes_info_json = JSON.parse_string(content)
+	var heroes_info_json = JSON.parse_string(content)
 	for hero_name in heroes_info_json:
 		var main_attribute = heroes_info_json[hero_name]["main_attribute"]
 		var role = heroes_info_json[hero_name]["role"]
 		var attack_type = heroes_info_json[hero_name]["attack_type"]
-		var new_hero = HeroInfo.new(hero_name, main_attribute, role, attack_type)
-		heroes.append(new_hero)
-	
-func setup_heroes_for_new_game():
-	for hero in heroes:
+		var hero = HeroInfo.new(hero_name, main_attribute, role, attack_type)
+		heroes.append(hero)
+		if main_attribute == "Strength":
+			strength_heroes.append(hero)
+		elif main_attribute == "Agility":
+			agility_heroes.append(hero)
+		else:
+			intelligence_heroes.append(hero)
 		hero.base_strength = get_attribute(heroes_info_json[hero.name], "base_strength")
 		hero.base_agility = get_attribute(heroes_info_json[hero.name], "base_agility")
 		hero.base_intelligence = get_attribute(heroes_info_json[hero.name], "base_intelligence")
@@ -52,6 +58,12 @@ func setup_heroes_for_new_game():
 		hero.base_attack_speed = get_attribute(heroes_info_json[hero.name], "base_attack_speed")
 		hero.base_move_speed = get_attribute(heroes_info_json[hero.name], "base_move_speed")
 		hero.base_attack_range = get_attribute(heroes_info_json[hero.name], "base_attack_range")
+
+func delete_all_heroes():
+	heroes.clear()
+	strength_heroes.clear()
+	agility_heroes.clear()
+	intelligence_heroes.clear()
 
 func get_attribute(attrs, attr_name):
 	var min_value_attr = int(attrs["min_%s" % attr_name])
--- Scripts/HeroesScreenController.gd
+++ Scripts/HeroesScreenController.gd
@@ -7,23 +7,35 @@ var hero_info_icon_prefab = preload("res://Prefabs/Hero Info Icon.tscn")
 @export var agility_heroes_table: Panel
 @export var intelligence_heroes_table: Panel
 
-func _ready():
-	var strength_count: int = 0
-	var agility_count: int = 0
-	var intelligence_count: int = 0
-	for hero_info in HeroesManager.heroes:
+func fix_heroes_icon_count():
+	for i in range(len(strength_heroes_table.get_children()), len(HeroesManager.strength_heroes)):
 		var new_hero_info_icon = hero_info_icon_prefab.instantiate() as HeroInfoButton
-		new_hero_info_icon.set_hero(hero_info)
 		new_hero_info_icon.connect("pressed", Callable(get_node("/root/Screen manager"), "show_hero"))
-		if hero_info.main_attribute == "Strength":
-			strength_heroes_table.add_child(new_hero_info_icon)
-			new_hero_info_icon.position = Vector2(119 * (strength_count % 4), 87 * (strength_count / 4))
-			strength_count += 1
-		elif hero_info.main_attribute == "Agility":
-			agility_heroes_table.add_child(new_hero_info_icon)
-			new_hero_info_icon.position = Vector2(119 * (agility_count % 4), 87 * (agility_count / 4))
-			agility_count += 1
-		else:
-			intelligence_heroes_table.add_child(new_hero_info_icon)
-			new_hero_info_icon.position = Vector2(119 * (intelligence_count % 4), 87 * (intelligence_count / 4))
-			intelligence_count += 1
+		strength_heroes_table.add_child(new_hero_info_icon)
+		new_hero_info_icon.position = Vector2(119 * (i % 4), 87 * (i / 4))
+	
+	for i in range(len(agility_heroes_table.get_children()), len(HeroesManager.agility_heroes)):
+		var new_hero_info_icon = hero_info_icon_prefab.instantiate() as HeroInfoButton
+		new_hero_info_icon.connect("pressed", Callable(get_node("/root/Screen manager"), "show_hero"))
+		agility_heroes_table.add_child(new_hero_info_icon)
+		new_hero_info_icon.position = Vector2(119 * (i % 4), 87 * (i / 4))
+		
+	for i in range(len(intelligence_heroes_table.get_children()), len(HeroesManager.intelligence_heroes)):
+		var new_hero_info_icon = hero_info_icon_prefab.instantiate() as HeroInfoButton
+		new_hero_info_icon.connect("pressed", Callable(get_node("/root/Screen manager"), "show_hero"))
+		intelligence_heroes_table.add_child(new_hero_info_icon)
+		new_hero_info_icon.position = Vector2(119 * (i % 4), 87 * (i / 4))
+	
+
+func update_heroes_table():
+	fix_heroes_icon_count()
+	var strength_heroes_icons = strength_heroes_table.get_children()
+	for i in range(len(HeroesManager.strength_heroes)):
+		strength_heroes_icons[i].set_hero(HeroesManager.strength_heroes[i])
+	var agility_heroes_icons = agility_heroes_table.get_children()
+	for i in range(len(HeroesManager.agility_heroes)):
+		agility_heroes_icons[i].set_hero(HeroesManager.agility_heroes[i])
+	var intelligence_heroes_icons = intelligence_heroes_table.get_children()
+	for i in range(len(HeroesManager.intelligence_heroes)):
+		intelligence_heroes_icons[i].set_hero(HeroesManager.intelligence_heroes[i])
+		
