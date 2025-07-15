2dcb0ae1782cae073ee308e1cb269d0b9cf7c4ef
Nikolai Nekrasov <nikolainekrasov@tessalite.com>
2025-07-15 04:10:44 +0300

    feat: add transition to hero screen from player screen

--- Scripts/HeroesSkillItem.gd
+++ Scripts/HeroesSkillItem.gd
@@ -10,11 +10,16 @@ class_name HeroesSkillItem
 @export var second_hero_skill: Label
 @export var first_hero_progress: TextureProgressBar
 @export var second_hero_progress: TextureProgressBar
-	
+@onready var screen_manager: ScreenManager = get_node("/root/Screen manager")
+@onready var player_screen: HeroScreenController = get_node("/root/Screen manager/Hero") as HeroScreenController
+
 func set_heroes(player, first_hero, second_hero):
 	first_hero_portrait.texture_normal = first_hero.mini_portrait
 	second_hero_portrait.texture_normal = second_hero.mini_portrait
-	first_hero_name.text = first_hero.name
+	first_hero_portrait.connect("pressed", Callable(screen_manager, "show_hero"))
+	second_hero_portrait.connect("pressed", Callable(screen_manager, "show_hero"))
+	first_hero_portrait.connect("pressed", Callable(player_screen, "set_hero").bind(first_hero))
+	second_hero_portrait.connect("pressed", Callable(player_screen, "set_hero").bind(second_hero))
 	second_hero_name.text = second_hero.name
 	first_hero_skill.text = str(player.heroes_skill[first_hero])
 	second_hero_skill.text = str(player.heroes_skill[second_hero])
