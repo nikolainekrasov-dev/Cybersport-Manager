855ab3a4866dfda134ea7e92e46be7c5f5460d88
Nikolai Nekrasov <nikolainekrasov@tessalite.com>
2025-07-16 04:00:22 +0300

    fix: fix bug with hero display

--- Scripts/HeroesSkillItem.gd
+++ Scripts/HeroesSkillItem.gd
@@ -13,15 +13,28 @@ class_name HeroesSkillItem
 @onready var screen_manager: ScreenManager = get_node("/root/Screen manager")
 @onready var player_screen: HeroScreenController = get_node("/root/Screen manager/Hero") as HeroScreenController
 
+var first_hero_info: HeroInfo
+var second_hero_info: HeroInfo
+
+func _ready():
+	first_hero_portrait.connect("pressed", Callable(self, "show_first_hero"))
+	second_hero_portrait.connect("pressed", Callable(self, "show_second_hero"))
+
 func set_heroes(player, first_hero, second_hero):
+	first_hero_info = first_hero
+	second_hero_info = second_hero
 	first_hero_portrait.texture_normal = first_hero.mini_portrait
 	second_hero_portrait.texture_normal = second_hero.mini_portrait
-	first_hero_portrait.connect("pressed", Callable(screen_manager, "show_hero"))
-	second_hero_portrait.connect("pressed", Callable(screen_manager, "show_hero"))
-	first_hero_portrait.connect("pressed", Callable(player_screen, "set_hero").bind(first_hero))
-	second_hero_portrait.connect("pressed", Callable(player_screen, "set_hero").bind(second_hero))
 	second_hero_name.text = second_hero.name
 	first_hero_skill.text = str(player.heroes_skill[first_hero])
 	second_hero_skill.text = str(player.heroes_skill[second_hero])
 	first_hero_progress.value = player.heroes_skill[first_hero]
 	second_hero_progress.value = player.heroes_skill[second_hero]
+
+func show_first_hero():
+	screen_manager.show_hero()
+	player_screen.set_hero(first_hero_info)
+	
+func show_second_hero():
+	screen_manager.show_hero()
+	player_screen.set_hero(second_hero_info)
