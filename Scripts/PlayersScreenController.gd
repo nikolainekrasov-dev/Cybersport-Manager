d53b135ca4cda38dc1ed6b831bfe99551934acab
Nikolai Nekrasov <nikolainekrasov@tessalite.com>
2025-07-18 02:43:14 +0300

    feat: change players screen

--- Scripts/PlayersScreenController.gd
+++ Scripts/PlayersScreenController.gd
@@ -2,5 +2,10 @@ extends CanvasLayer
 
 class_name PlayersScreenController
 
+var players_table: PlayersTable
+
+func _ready():
+	players_table = find_child("Players Container")
+
 func update():
-	pass
+	players_table.update_table_content()
