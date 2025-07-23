dca0f19113fd06d69b311514d906ae34aca41bbb
Nikolai Nekrasov <nikolainekrasov@tessalite.com>
2025-07-23 07:20:03 +0300

    fix: change LabeledTextureButton class

--- Scripts/LabeledTextureButton.gd
+++ Scripts/LabeledTextureButton.gd
@@ -2,23 +2,20 @@ extends TextureButton
 
 class_name LabeledTextureButton
 
-var labels = []
-@export var default_text_color = Color("#636363")
-@export var hover_text_color = Color.WHITE
+@export var labels: Array[Label]
+@export var colors: Array[Array]
+
 
 func _ready():
-	for child in get_children():
-		if child is Label:
-			labels.append(child)
 	connect("mouse_entered", Callable(self, "set_hover_text_color"))
 	connect("mouse_exited", Callable(self, "set_default_text_color"))
 	connect("focus_exited", Callable(self, "set_default_text_color"))
 			
 func set_default_text_color():
 	if not(has_focus()):
-		for label in labels:
-			label.add_theme_color_override("font_color", default_text_color)
+		for i in range(len(labels)):
+			labels[i].add_theme_color_override("font_color", colors[i][0])
 		
 func set_hover_text_color():
-	for label in labels:
-			label.add_theme_color_override("font_color", hover_text_color)
+	for i in range(len(labels)):
+		labels[i].add_theme_color_override("font_color", colors[i][1])
